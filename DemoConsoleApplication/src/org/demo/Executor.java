1262e010d85a342e720fc5988fdecde1ee0d043b
lucas49 <saurabhdeshpande49@gmail.com>
2014-05-15 00:50:53 +0530

    adding file to git

---
+++ DemoConsoleApplication/src/org/demo/Executor.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.demo;
+
+import java.util.Scanner;
+
+/**
+ * @author Satts
+ *
+ */
+public class Executor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter a number:");
+		Scanner scn = new Scanner(System.in);
+		int myInt = scn.nextInt();
+		System.out.println("Entered number is: " + myInt);
+	}
+
+}
